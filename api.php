f1da58da070963f40713eb67f30beb7a7738d634
Yersy Aliaga <yaliagap@pe.ibm.com>
2016-11-23 10:14:04 -0600

    que pasaaaaa

--- api.php
+++ api.php
@@ -41,7 +41,7 @@ function getActividades($conn){
 }
 
 function insertEmergency($conn,$userId,$latitud,$longitud){
-	$query = "insert into emergencias(idUsuario,fechaHora,latitud,longitud,estado) values ($userId,now(),$latitud,$longitud,'En Proceso');";
+	$query = "insert into emergencias(idUsuario,fechaHora,latitud,longitud,estado) values (1,now(),$latitud,$longitud,'En Proceso');";
 	if (mysqli_query($conn, $query)) {
 			    echo "New record created successfully";
 	} else {

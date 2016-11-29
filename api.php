fa52fdcc0e7cbb350795c7c7c3eda0ee92e6a31f
Yersy Aliaga <yaliagap@pe.ibm.com>
2016-11-29 12:49:55 -0600

    register Push Notifications

--- api.php
+++ api.php
@@ -4,6 +4,72 @@ include("db.php");
 $method = $_SERVER['REQUEST_METHOD'];
 mysqli_set_charset($conn,"utf8");
 
+
+//Funciones para el registro de Push y envío de notificaciones Push
+
+function registerPushNotifications($conn, $user, $device_id) {
+    $query = mysqli_query($conn, "SELECT * FROM usuarios WHERE idDevice='".$device_id ."' and username='".$user ."' ");
+	if (mysqli_num_rows($query) > 0){	
+	    return array("success" => 1);
+	} else {	    
+	    $sql = "UPDATE usuarios SET idDevice='".$device_id."' where username='".$user."';"; 
+	    if (!mysqli_query($conn, $query)) {
+	    	return array("success" => 0, "Falla Multiquery");
+	    }
+	}
+	return array("success" => 1);
+}
+
+function sendPushNotifications($conn, $apiKey, $appsecret, $message) {
+	$idsSelect = "select A.propiedad FROM (SELECT distinct lower(propiedad) as propiedad FROM tareas where (estado = 'Nuevo' or estado='En desarrollo') and sprint = (select max(sprint) from tareas)) A left join (SELECT count(*) as cantidad, propiedad FROM tareas where (estado='Nuevo' or estado='En desarrollo') and sprint = (select max(sprint) from tareas) group by propiedad order by count(*) asc) B on A.propiedad = B.propiedad";   
+    $sql = "select * from registros_push WHERE user in ($idsSelect)"; 
+
+	$result = mysqli_query($conn, $sql);
+
+	$device_ids = array();
+	if (mysqli_num_rows($result) > 0) {
+	    while($row = mysqli_fetch_assoc($result)) {
+	    	array_push($device_ids, $row["device_id"]);
+	    }
+	} else {
+	    echo "0 results";
+	}	
+    $device_ids_j = json_encode($device_ids);
+    
+    if (count($device_ids) > 0) {
+
+		$data_string ='{"message": { "alert": "'. $message .'" }, "target" : {"deviceIds" :' . $device_ids_j . ' } }';
+			
+		$ch = curl_init('https://mobile.ng.bluemix.net/imfpush/v1/apps/' . $apiKey .'/messages');
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+		    'Content-Type: application/json',                                                                                
+		    'Content-Length: ' . strlen($data_string),
+		    'Accept: application/json',
+		    'appSecret:'. $appsecret, 
+		    'Accept-Language: en-US'      )                                                          
+		);               
+	
+		$text1 = curl_exec($ch);
+		if (FALSE === $text1) {
+	       print curl_error($ch);
+	       print curl_errno($ch);
+	   }
+	   var_dump( $text1);
+	   curl_close($ch);	
+	}	 
+
+}
+
+
+//Fin_Parte de funciones push
+
+
+
+
+
 function validarIngreso($conn, $user,$pass) {
 	$query = mysqli_query($conn,"Select username from usuarios where username='".$user."' and password='".$pass."'");
 	$cadena = array();
@@ -93,7 +159,6 @@ function insertEmergency($conn,$userId,$latitud,$longitud){
 }
 
 
-
 switch ($method) {
   case 'POST':
 	  if (isset($_POST["procedure"])) {
@@ -115,6 +180,11 @@ switch ($method) {
 	  			$procuradorId = $_POST["procuradorid"];
 	  			$emergenciaId = $_POST["emergenciaid"];
 	  			actualizarProcurador($conn,$procuradorId,$emergenciaId);
+	  			break;
+	  		case '4':
+	  			$user = $_POST["username"];
+	  			$device_id = $_POST["deviceid"];
+	  			registerPushNotifications($conn,$user,$device_id);
 	  			break;
 	  	}
 	  }
